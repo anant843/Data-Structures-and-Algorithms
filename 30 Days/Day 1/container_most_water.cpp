9ed499dc971b9a35b53c54db7bf8724803472334
Anant Singh Shishodia <anantthakur2005@gmail.com>
2026-06-17 23:55:14 +0530

    Container with Most Water

---
+++ 30 Days/Day 1/container_most_water.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int l=0;
+    int r=n-1;
+    int maxwater=0;
+    while(l<r){
+    int w=r-l;
+    int h=min(arr[l],arr[r]);
+    int area=w*h;
+    maxwater=max(area,maxwater);
+    if(arr[l]<arr[r]){
+        l++;
+    }
+    else if(arr[l]>arr[r]){
+        r--;
+    }
+    else{
+        l++;
+        r--;
+    }
+} 
+cout<<maxwater;   
+}
+
+// Input...
+// 9
+// 1 8 6 2 5 4 8 3 7
+// Output... 
+// 49
