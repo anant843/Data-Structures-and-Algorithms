6da6d42c6dae8a31615d59ecaa978f9e175b64ad
Anant Singh Shishodia <anantthakur2005@gmail.com>
2026-08-08 02:20:38 +0530

    Length of Longest Palindrome Subsequence

---
+++ Dynamic Programming/Knapsack Variation/15_Longest_Palindrome_subsequence.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Memoization
+
+int Lcs(string s, string s1, int n, int m, vector<vector<int>>& dp)
+{
+    // Base Case
+    if(n == 0 || m == 0)
+        return 0;
+
+    // Already Computed
+    if(dp[n][m] != -1)
+        return dp[n][m];
+
+    // Match
+    if(s[n-1] == s1[m-1])
+    {
+        dp[n][m] = 1 + Lcs(s, s1, n-1, m-1, dp);
+    }
+    // Not Match
+    else
+    {
+        dp[n][m] = max(
+            Lcs(s, s1, n-1, m, dp),
+            Lcs(s, s1, n, m-1, dp)
+        );
+    }
+
+    return dp[n][m];
+}
+
+/*
+
+//Tabulation
+
+int Lcs(string s, string s1)
+{
+    int n = s.length();
+    int m = s1.length();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= m; j++)
+        {
+            if(s[i-1] == s1[j-1])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j] = max(
+                    dp[i-1][j],
+                    dp[i][j-1]
+                );
+            }
+        }
+    }
+
+    return dp[n][m];
+}
+
+*/
+
+int main()
+{
+    string s;
+
+    cout << "Enter first string: ";
+    cin >> s;
+
+   string s1 = s;
+   reverse(s1.begin(), s1.end());
+   
+    int n = s.length();
+    int m = s1.length();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
+    cout << "Length of Longest Palindrome Subsequence: "
+         << Lcs(s, s1, n, m, dp);
+
+    return 0;
+}
+
+// //Input...
+// Enter first string: agbcba
+
+// //Output...
+// Length of Longest Palindrome Subsequence: 5
