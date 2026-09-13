e6452abc145bac57e5ca0ea2be6d10a293606535
Anant Singh Shishodia <anantthakur2005@gmail.com>
2026-09-13 09:23:39 +0530

    Sort Colors

---
+++ 30 Days/Day7/Sort_colors.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+      int countzero=0;
+      int countone=0;
+      int counttwo=0;
+
+      for(int x:nums){
+        if(x==0)countzero++;
+        else if(x==1)countone++;
+        else counttwo++;
+      }
+
+      int i=0;
+
+      while(countzero--){
+        nums[i++]=0;
+      }
+
+      while(countone--){
+        nums[i++]=1;
+      }
+
+      while(counttwo--){
+        nums[i++]=2;
+      }
+    }
+};
+
+//optimal Dutch national flag algorithm
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+
+        while(mid<=high){
+
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+
+            else if(nums[mid]==1){
+                mid++;
+            }
+
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};
